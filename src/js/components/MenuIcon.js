f1a35ac5cb23eaf768a36c45741b184c934c4dbd
Marcin Hercog <mkp.hercog@gmail.com>
2020-02-11 19:49:33 +0100

    change navigation and icon animation with JS

--- src/js/components/MenuIcon.js
+++ src/js/components/MenuIcon.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/MenuIcon.scss";
 
-const MenuIcon = props => {
+const MenuIcon = ({ showMenu, click }) => {
+  const [positionIcon, setPositionIcon] = useState(0);
+  const animationSpeed = 10;
+
+  function iconSlide(num) {
+    setPositionIcon(positionIcon + `${num}` * 1);
+  }
+
+  if (!showMenu && positionIcon < 0) {
+    setTimeout(() => iconSlide(1), animationSpeed);
+  }
+  if (showMenu && positionIcon > -100) {
+    setTimeout(() => iconSlide(-1), animationSpeed);
+  }
+
   return (
     <div
-      onClick={props.click}
-      className={`menu-icon ${props.showMenu ? "hide" : "show"}`}
+      onClick={click}
+      className="menu-icon"
+      style={{ right: positionIcon + "px" }}
     >
       <i className="fas fa-caret-down"></i>
     </div>
--- src/js/layouts/Navigation.js
+++ src/js/layouts/Navigation.js
@@ -34,6 +34,46 @@ const navItems = [
 
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [positionMenu, setPositionMenu] = useState(-100);
+  const animationSpeed = 10;
+
+  //menu animation when mobile or width < 1024px
+  function menuSlide(num) {
+    setPositionMenu(positionMenu + `${num}` * 1);
+  }
+
+  if (showMenu && positionMenu < 0) {
+    setTimeout(() => menuSlide(1), animationSpeed);
+  }
+  if (!showMenu && positionMenu > -100) {
+    setTimeout(() => menuSlide(-1), animationSpeed);
+  }
+  //--
+
+  //initial look of menu (when web load and window resize)
+  //I must do here setPositionMenu, because on desktop I need 'right: auto'
+  //And after resize window < 1024px value must back to Number
+  window.onload = () => {
+    if (window.innerWidth >= 1024 && !showMenu) {
+      setPositionMenu("auto");
+      setShowMenu(true);
+    } else {
+      setPositionMenu(-100);
+      setShowMenu(false);
+    }
+
+    window.addEventListener("resize", () => {
+      let windowWidth = window.innerWidth;
+      if (windowWidth >= 1024) {
+        setPositionMenu("auto");
+        setShowMenu(true);
+      } else if (windowWidth < 1024) {
+        setPositionMenu(-100);
+        setShowMenu(false);
+      }
+    });
+  };
+  // --
 
   const nav = navItems.map(item => (
     <li className="nav__item" key={item.name}>
@@ -41,6 +81,7 @@ const Navigation = () => {
         className="nav__link"
         to={item.path}
         exact={item.exact ? item.exact : false}
+        onClick={() => setShowMenu(false)}
       >
         {item.name}
       </NavLink>
@@ -49,7 +90,12 @@ const Navigation = () => {
 
   return (
     <>
-      <ul className={`nav ${showMenu ? "show" : "hide"}`}>
+      <ul
+        className="nav"
+        style={{
+          right: [positionMenu === "auto" ? positionMenu : positionMenu + "vw"]
+        }}
+      >
         <ExitIcon click={() => setShowMenu(false)} />
         {nav}{" "}
       </ul>
