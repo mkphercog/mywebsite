3198579c9f9978027d2ea4bd0a86621ee3952c4b
Marcin Hercog <mkp.hercog@gmail.com>
2020-03-05 19:24:19 +0100

    add web-informations.json add info from json in about section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./About.scss";
+import INFO from "../../web-informations.json";
 
 const yearNow = new Date().getFullYear();
 const yearOfMyBirth = 1993;
-
-const textPL = `Nazywam się Marcin Hercog i mam ${yearNow -
-  yearOfMyBirth} lat. Swoją przygodę z programowaniem zacząłem już w technikum, jednak po drodze wiele spraw przeszkadzało mi w skupieniu się wyłącznie na tej pasji. W szkole zaznajomiłem się z Pascalem i C++, a po jej ukończeniu, już we własnym zakresie, z C# i Javą. Nie była to jednak wiedza na poziomie, który pozwoliłby rozpocząć pracę zawodową, dlatego też programowanie zawsze było gdzieś na drugim planie. Na początku września 2019 postanowiłem wrócić do programowania. Tym razem postawiłem na HTML, CSS i, za namową znajomego, JavaScript (+ React). Jest to mój wielki powrót do programowania po latach, z czego bardzo się cieszę! :)`;
-
-const textEN = `Translation in near future... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ratione possimus, temporibus nisi vero laborum officia animi quos dicta deserunt quisquam id obcaecati totam quasi perferendis natus incidunt explicabo debitis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ratione possimus, temporibus nisi vero laborum officia animi quos dicta deserunt quisquam id obcaecati totam quasi perferendis natus incidunt explicabo debitis.`;
+const myAge = yearNow - yearOfMyBirth;
+const { PL_HELLO, PL_DESCRIPTION1, PL_DESCRIPTION2 } = INFO.PL.ABOUT;
+const { EN_HELLO, EN_DESCRIPTION1, EN_DESCRIPTION2 } = INFO.EN.ABOUT;
 
 export const About = props => (
   <div className="about">
     <div className="about__foto"></div>
     <div className="about__wrapText">
       <h1 className="about__welcome">
-        {props.lang === "PL" ? "Witaj!" : "Hello!"}
+        {props.lang === "PL" ? PL_HELLO : EN_HELLO}
       </h1>
       <p className="about__description">
-        {props.lang === "PL" ? textPL : textEN}
+        {props.lang === "PL"
+          ? `${PL_DESCRIPTION1}${myAge}${PL_DESCRIPTION2}`
+          : `${EN_DESCRIPTION1}${myAge}${EN_DESCRIPTION2}`}
       </p>
     </div>
   </div>
