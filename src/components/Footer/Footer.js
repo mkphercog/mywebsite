26eec2028d995ab66dc3bf83fe08b1098b24deb2
Marcin Hercog <mkp.hercog@gmail.com>
2020-08-15 14:40:22 +0200

    delete current web from portfolio add link to web code in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,6 +18,14 @@ export const Footer = ({ selectedLanguage }) => {
       <p className="footer__name">
         &copy; {isPolishLanguageChoosen ? PL_NAME : EN_NAME} Hercog
       </p>
+      <a
+        className="footer__web-code"
+        href="https://github.com/mkphercog/mywebsite/tree/master"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {isPolishLanguageChoosen ? "{ Kod strony }" : "{ Website code }"}
+      </a>
     </footer>
   );
 };
